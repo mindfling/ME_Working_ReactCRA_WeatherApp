692c8a133ec40be12d20a0e7f438aef0dfbc4bd3
Nepochtov Dmitry <dimendroider@gmail.com>
2023-10-14 01:01:47 +0300

    add Container component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import style from "./App.module.scss";
+import { Container } from "./modules/Container/Container";
 
 
 function App() {
   const myTitle: string = 'Hello create React App';
   return (
     <div className={style.app}>
+      <Container/>
       {myTitle}
     </div>
   );
---
+++ src/modules/Container/Container.tsx
@@ -0,0 +1,13 @@
+// tsrafc
+import React from 'react'
+import style from './Container.module.scss';
+
+type Props = {}
+
+export const Container = (props: Props) => {
+  return (
+    <div className={style.container}>
+      Container
+    </div>
+  )
+}
